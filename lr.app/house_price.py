95340b0bf7c477413a63b4987f994823b8bf550b
chaudhary-dotcom <oends38@gmail.com>
2025-03-23 22:48:34 +0545

    lr4

---
+++ lr.app/house_price.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
